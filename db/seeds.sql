6924f3364efb10eeef04ce88a525ebdb75e5ddef
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-06-06 23:28:59 -0600

    add cases to display and code for add employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,10 +15,11 @@ VALUES  ('Salesman', '60000', 1),
 
 INSERT INTO employees (first_name,last_name, title_id, manager_id)
 VALUES  ('Aime', 'Rangel',4, null),
-        ('Snella', 'Von Parda', 1,1),
+        ('Snella', 'Von Parda', 1,null),
+        ('Xixa','Calicq',6, null),
         ('Huantonio', 'C.R.', 2,1),
-        ('Komilia', 'Flotante',3,1),
-        ('Raja', 'Orange',5,1),
-        ('Megan', 'Ghost',1,2),
-        ('Xixa','Calicq',6,2),
-        ('Jenny','Cow',2,3);
+        ('Komilia', 'Flotante',3,2),
+        ('Raja', 'Orange',5,3),
+        ('Megan', 'Ghost',1,3),
+        ('Miela', 'Cabrera',1,2),
+        ('Jenny','Cow',2,1);
--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const logo = require('asciiart-logo');
 const config = require('./package.json');
 const inquirer = require('inquirer'); // Package for prompting user input
 const fs = require('fs'); // File system module
-
+const mysql = require('mysql2');
 console.log(logo(config).render());
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'AsMySQL.2211',
+      database: 'employee_management_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
 
 const options = [
     {name: "View All Employees",               value: "viewAll"},
@@ -23,26 +35,211 @@ const options = [
     {name: "Quit",                             value: "quit"}
 ]
 
-const menu = () => {
-    return inquirer.prompt([
-        {
-        type: "list",  
-        name: "menu",     
-        message: "What would you like to do?",        
-        choices: options,
-
-        }
+function promptNewEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'Enter the first name of the employee:',
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'Enter the last name of the employee:',
+      },
+      {
+        type: 'input',
+        name: 'title_id',
+        message: 'Enter the title ID of the employee:',
+      },
+      {
+        type: 'input',
+        name: 'manager_id',
+        message: 'Enter the manager ID of the employee (leave blank if none):',
+      },
     ])
+    .then(answers => {
+      // Handle the new employee data here
+      const { first_name, last_name, title_id, manager_id } = answers;
+
+      // Perform the database query to insert the new employee
+      const query = `INSERT INTO employees (first_name, last_name, title_id, manager_id) VALUES (?, ?, ?, ?)`;
+      const values = [first_name, last_name, title_id, manager_id];
+
+      connection.query(query, values, (error, results) => {
+        if (error) {
+          console.error('Error occurred while adding the employee:', error);
+        } else {
+          console.log('New employee added:');
+          console.log('First Name:', first_name);
+          console.log('Last Name:', last_name);
+          console.log('Title ID:', title_id);
+          console.log('Manager ID:', manager_id || 'None');
+        }
+      });
+    })
+    .catch(error => {
+      console.error('Error occurred:', error);
+    });
 }
 
+const mainMenu = () => {
+    return inquirer.prompt([
+        { type:"list", name:"menu", message:"What would you like to do?", choices: options,}
+    ])
+    .then((menuOption) => {
+        console.log(menuOption);
+        selectedOption(menuOption);
+    })
+}
 
+const selectedOption = ({menu}) =>{
+    switch (menu) {
+        case "viewAll":
+            db.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employees e JOIN roles r ON e.title_id = r.id LEFT JOIN departments d ON r.department_id = d.id LEFT JOIN employees m ON e.manager_id = m.id ORDER BY e.id ASC;`, function (err, results) {
+                console.log("\n")
+                console.table(results);
+                console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+          break;
+        case "viewAllbyDepartment":
+          db.query(`SELECT e.id, e.first_name, e.last_name, d.department_name
+          FROM employees e
+          JOIN roles r ON e.title_id = r.id
+          JOIN departments d ON r.department_id = d.id
+          ORDER BY d.department_name;`, function (err, results) {
+            console.log("\n")
+            console.table(results);
+            console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+          break;
+        case "viewAllbyManager":
+          db.query(`SELECT e.id, e.first_name, e.last_name, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+          FROM employees e
+          LEFT JOIN employees m ON e.manager_id = m.id
+          ORDER BY manager_name;`, function (err, results) {
+            console.log("\n")
+            console.table(results);
+            console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+          break;
+        case "add":
+          inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'first_name',
+              message: 'Enter the first name of the employee:',
+            },
+            {
+              type: 'input',
+              name: 'last_name',
+              message: 'Enter the last name of the employee:',
+            },
+            {
+              type: 'input',
+              name: 'title_id',
+              message: 'Enter the title ID of the employee:',
+            },
+            {
+              type: 'input',
+              name: 'manager_id',
+              message: 'Enter the manager ID of the employee (leave blank if none):',
+            },
+          ])
+          .then(answers => {
+            // Handle the new employee data here
+            const { first_name, last_name, title_id, manager_id } = answers;
+      
+            // Perform the database query to insert the new employee
+            const query = `INSERT INTO employees (first_name, last_name, title_id, manager_id) VALUES (?, ?, ?, ?)`;
+            const values = [first_name, last_name, title_id, manager_id];
+      
+            db.query(query, values, (error, results) => {
+              if (error) {
+                console.error('Error occurred while adding the employee:', error);
+              } else {
+                console.log('New employee added:');
+                console.log('First Name:', first_name);
+                console.log('Last Name:', last_name);
+                console.log('Title ID:', title_id);
+                console.log('Manager ID:', manager_id || 'None');
+              }
+              // Close the database connection
+              db.end();
+            });
+          })
+          .catch(error => {
+            console.error('Error occurred:', error);
+          });
+          break;
+        case "remove":
+             
+        break;
+        case "updateRole":
+             
+        break;
+        case "updateManager":
+             
+        break;
+        case "viewAllRoles":
+          db.query(`SELECT r.title AS role, d.department_name AS department, r.salary
+          FROM roles r
+          JOIN departments d ON r.department_id = d.id;`, function (err, results) {
+            console.log("\n")
+            console.table(results);
+            console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+        break;
+        case "addRole":
+             
+        break;
+        case "removeRole":
+             
+        break;
+        case "viewAllDepartments":
+          db.query(`SELECT 
+          d.id AS DepartmentID,
+          d.department_name AS Department
+        FROM departments d
+        ORDER BY DepartmentID;`, function (err, results) {
+            console.log("\n")
+            console.table(results);
+            console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+        break;
+        case "addDepartment":
+             
+        break;
+        case "removeDepartment":
+             
+        break;
+        case "viewAllBudget":
+          db.query(`SELECT
+          d.department_name,
+          SUM(r.salary) AS total_budget
+        FROM employees e
+        JOIN roles r ON e.title_id = r.id
+        LEFT JOIN departments d ON r.department_id = d.id
+        GROUP BY d.department_name;`, function (err, results) {
+            console.log("\n")
+            console.table(results);
+            console.log("\n\n\n\n\n\n\n\n\n\n")
+          });
+        break;
+        case "quit":
+        break;            
+        default:
+        console.log("\n")
+        break;
+    }
+
+    mainMenu();
+}
 
 function init(){
-menu()
-.then((menuOption) => {
-    console.log(menuOption);
-}
+    mainMenu()
 
-)
 }
 init(); // Function call to initialize app
